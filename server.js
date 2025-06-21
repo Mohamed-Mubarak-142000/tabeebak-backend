a50e6b7c4d62a12986b8ada213669b9040753249
mmubarak.innovatek <mmubarak.innovatek@outlook.com>
2025-06-21 19:40:45 +0300

    first commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://your-frontend-app.vercel.app"],
   credentials: true,
   optionsSuccessStatus: 200,
 };
